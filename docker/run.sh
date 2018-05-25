9c5fbc323cc79fece86bea3799fa476460b2b54f
tabhan <tabhan@aaxiscommerce.com>
2018-05-26 07:33:32 +0800

    DevMod

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,7 @@ touch app/logs/prod.log
 touch app/logs/dev.log
 chgrp -R www-data .
 chmod -R g+w app/cache app/logs
+chmod -R a+x app/console
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* app/logs/prod.log app/logs/dev.log &
 /project/app/console server:start
