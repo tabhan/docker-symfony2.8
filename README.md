3d6db7dee1e5a4328019e3147ff262fb61123856
tabhan <tabhan@aaxiscommerce.com>
2018-05-26 02:40:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+myproject_symfony2
+==================
+
+A Symfony project created on May 25, 2018, 4:44 pm.
---
+++ docker/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p app/cache app/logs
+touch app/logs/prod.log
+touch app/logs/dev.log
+chgrp -R www-data .
+chmod -R g+w app/cache app/logs
+source /etc/apache2/envvars
+tail -F /var/log/apache2/* app/logs/prod.log app/logs/dev.log &
+exec apache2 -D FOREGROUND
